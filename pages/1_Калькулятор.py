72cebef993503a1a303576fbb0bbaf2c1d287ee3
Ivan Sharun <ivan@sha.run>
2023-11-17 08:42:09 +0600

    Update features

--- pages/1_Калькулятор.py
+++ pages/1_Калькулятор.py
@@ -17,18 +17,22 @@ st.write("# Анализ наличия/отсутствия ЖЭС")
 data = {}
 data['Возраст'] = st.slider('Возраст', min_value=10, max_value=59, value=14)
 data['Рост,м'] = st.slider('Рост, м', min_value=1.2, max_value=2.0, value=1.86)
-data['ЧСС ночью Макс'] = st.slider('ЧСС ночью, макс', min_value=69, max_value=149, value=98)
-data['САДd'] = st.slider('САДd', min_value=88, max_value=136, value=102)
-data['САДs'] = st.slider('САДs', min_value=90, max_value=134, value=100)
-data['Кол-во эпизодов синусовой тахикардии'] = st.slider('Кол-во эпизодов синусовой тахикардии', min_value=0, max_value=235, value=32)
-data['pNN50 день'] = st.slider('pNN50, день', min_value=0.9, max_value=33.1, value=19.0)
-data['Эпизоды сердцебиений'] = st.selectbox('Эпизоды сердцебиений', ['Нет', 'Да'])
+data['Вес'] = st.slider('Вес, кг', min_value=26.0, max_value=85.0, value=56.0)
+
+data['ИМТ'] = st.slider('ИМТ', min_value=13.0, max_value=27.0, value=16.19)
 data['Индекс Варге (ИВ)'] = st.slider('Индекс Варге (ИВ)', min_value=1.08, max_value=2.5, value=1.479)
+
+data['Эпизоды сердцебиений'] = st.selectbox('Эпизоды сердцебиений', ['Нет', 'Да'])
+data['Головокружение'] = st.selectbox('Головокружение', ['Нет', 'Да'])
+
+data['САДd'] = st.slider('САДd', min_value=88.0, max_value=136.0, value=102.0)
+data['САДs'] = st.slider('САДs', min_value=90.0, max_value=134.0, value=100.0)
+
 data['Итог'] = st.slider('Фены, Итог', min_value=3.99, max_value=87.83, value=61.44)
 
+
 def models_report(data):
-    keys = ['Рост,м', 'ЧСС ночью Макс', 'САДd', 'САДs',
-       'Кол-во эпизодов синусовой тахикардии', 'pNN50 день',
+    keys = ['Вес', 'Головокружение', 'Рост,м', 'САДd', 'ИМТ', 'САДs',
        'Эпизоды сердцебиений', 'Индекс Варге (ИВ)', 'Итог', 'Возраст']
     df = pd.Series(data).to_frame().T
     df = df[keys]
@@ -58,4 +62,6 @@ def models_report(data):
 
 if st.button('Получить результат'):
     data['Эпизоды сердцебиений'] = 1 if data['Эпизоды сердцебиений'] == 'Да' else 0
+    data['Головокружение'] = 1 if data['Головокружение'] == 'Да' else 0
+    
     models_report(data)
